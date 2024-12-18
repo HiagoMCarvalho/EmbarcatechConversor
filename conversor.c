cc398447b1ecc1748a4fffa2087e715a4356845a
Hiago Moura <hiago.moura7@gmail.com>
2024-12-18 16:29:48 -0300

    passagem da funcao de conversao pra um header

--- conversor.c
+++ conversor.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include "distancia.h"
 
 
-double obterFatorConversao(char unidade[]) {
-    if (strcmp(unidade, "metro") == 0) return 1.0;
-    if (strcmp(unidade, "centimetro") == 0) return 100.0;
-    if (strcmp(unidade, "quilometro") == 0) return 0.001;
-    if (strcmp(unidade, "milimetro") == 0) return 1000.0;
-    if (strcmp(unidade, "milha") == 0) return 0.000621371;
-    if (strcmp(unidade, "jarda") == 0) return 1.09361;
-    if (strcmp(unidade, "pe") == 0) return 3.28084;
-    if (strcmp(unidade, "polegada") == 0) return 39.3701;
-
-    // Unidade desconhecida
-    return -1.0;
-}
 int main()
 {
    int unidade;
+   double valor;
+   char unidadeOrigem[20], unidadeDestino[20];
 
    printf("Digite o numero relacionado a unidade que deseja converter: \n");
    printf("1. distancia (metro, centimetro...)\n");
@@ -28,9 +18,7 @@ int main()
    switch (unidade)
    {
    case 1:
-    double valor;
-    char unidadeOrigem[20], unidadeDestino[20];
-
+   
     printf("Digite o valor a ser convertido: ");
     scanf("%lf", &valor);
 
