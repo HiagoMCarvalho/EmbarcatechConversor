e82d8e468a3d66b174bb29a545168d6be4e5a032
IkaroJS <Ikarojesusdasilva@outlook.com>
2024-12-20 19:27:26 -0300

    Alterações de unidades de massa.h

--- massa.h
+++ massa.h
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include <string.h>
 
-double obterFatorConversaoDistancia(char unidade[]);
+double obterFatorConversaoDistancia(double valor, char unidade[]);
 
 double obterFatorConversaoMassa(char unidade[])
 {
+if (strcmp(unidade, "quilograma") == 0) return 1.0;
+
+if(strcmp(unidade, "grama") == 0) return 1000;
+
+if (strcmp(unidade, "miligrama")) return 1000000;
+
+if (strcmp(unidade, "tonelada")) return /*valor*/ 1000000;
 
     return 0;
 }
