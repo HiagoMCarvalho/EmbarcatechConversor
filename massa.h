0f30ccad84943f25e60f8c4ebd542f4ebca311d8
IkaroJS <Ikarojesusdasilva@outlook.com>
2024-12-21 15:14:08 -0300

    Alteração na lógica de conversão

--- massa.h
+++ massa.h
@@ -3,6 +3,7 @@
 double obterFatorConversaoMassa(char unidade[]);
 
 double obterFatorConversaoMassa(char unidade[])
+//Valores com padrão de quilograma
 {
     if (strcmp(unidade, "quilograma") == 0)
         return 1.0;
@@ -15,18 +16,6 @@ double obterFatorConversaoMassa(char unidade[])
 
     if (strcmp(unidade, "tonelada") == 0)
         return 0.000001;
-
-    double converterMassa(double valor, char origem[], char destino[]){
-        double fatorOrigem = obterFatorConversaoMassa(origem);
-        double fatorDestino = obterFatorConversaoMassa(destino);
-
-        if (fatorOrigem == 0 || fatorDestino == 0)
-        {
-            return -1;
-        }
-
-        return valor * (fatorOrigem / fatorDestino);
-    }
 
     return 0;
 }
