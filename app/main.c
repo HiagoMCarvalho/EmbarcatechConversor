fd291bb01c2e1a266e61a03ad918f72b856abed1
Vinícius Ribeiro <vinir14.p5@gmail.com>
2024-12-26 19:03:31 -0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -281,6 +281,41 @@ int main()
         
     break;
     }
+
+case 9: {
+
+int Litros_para_ml(int litros){
+    return litros * 1000;
+}
+double Litros_para_mcubico(int litros){
+    return litros / 1000.0;
+}
+
+    char unidade[20];
+    int litros;
+
+    printf("Digite a unidade para conversao (mililitros ou mcubico): ");
+    scanf("%s", unidade);
+
+    // verificar qual unidade foi escolhida e realizar a conversão
+    if (strcmp(unidade, "mililitros") == 0) {
+        printf("Digite a quantidade de litros: ");
+        scanf("%d", &litros);
+        printf("%d litros em mililitros: %d\n", litros, Litros_para_ml(litros));
+    } 
+    else if (strcmp(unidade, "mcubico") == 0) {
+        printf("Digite a quantidade de litros: ");
+        scanf("%d", &litros);
+        printf("%d litros em metros cubicos: %lf\n", litros, Litros_para_mcubico(litros));
+    } 
+    else {
+        printf("Unidade invalida. Por favor, insira 'mililitros' ou 'mcubico'.\n");
+        return 1;
+    }
+}
+
+    return 0;
+    break;
     
     default:
         printf("unidade fora do escopo que pode ser analisado\n");
